b78c1fb4a53923c7a19a8e90b8e1c5f2a4149cbc
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-10-12 11:38:59 -0500

    cotrollers in progress

--- controller/controller.go
+++ controller/controller.go
@@ -1,2 +1,24 @@
-
 package controller
+
+import (
+	"aalsaedi265/slack_bot/database"
+	"aalsaedi265/slack_bot/entity"
+	"encoding/json"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func GetAllStudent(w http.ResponseWriter, r *http.Request){
+	var students []entity.Student
+	database.Connector.Find(&students)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(students)
+}
+
+func GetStudentsByID(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	key := vars["Id"]
+}
